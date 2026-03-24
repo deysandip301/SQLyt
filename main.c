7d8605bfc9a8af1adeb9f15646afaa70487b6552
deysandip301 <deysandip301@gmail.com>
2026-03-24 13:34:32 +0530

    initial commit obv it won't compile :(

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+    while(true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if(strcmp(input_buffer->buffer,".exit")==0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            printf("Unrecognized command '%s'.\n",input_buffer->buffer);
+        }
+    }
+}
